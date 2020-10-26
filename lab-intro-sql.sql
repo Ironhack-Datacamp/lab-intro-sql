4725f4095a7795eeb8b81c97cb86d7225685b3b2
Carbonnell <julien.carbonnell@gmail.com>
2020-10-26 16:54:12 +0100

    Add files via upload

---
+++ lab-intro-sql.sql
@@ -0,0 +1,13 @@
+# 2. select * from sakila.`actor`,sakila.`address`,sakila.`category`, ... 
+
+# 3. select `title` from `film_list`
+
+# 4. select `name` from `language` as languag
+
+# 5. select COUNT(DISTINCT(`store_id`)) from `store` as store_total
+ 
+# 5. select COUNT(DISTINCT(`staff_id`)) from `staff` as staff_total
+
+# 5. select `first_name` from `staff`
+
+# Bonus : select COUNT(DISTINCT`rental_date`) from `rental`
